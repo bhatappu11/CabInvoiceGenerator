5b1f81513d833d76ff8049de8b9b14444f46384e
Arpitha Bhat <arpithabht12@gmail.com>
2021-09-22 20:14:40 +0530

    ADDED: method to get invoice summary based on userId

--- CabInvoiceGenerator/src/main/java/com/bridgelabz/cabinvoicegenerator/CabInvoiceGenerator.java
+++ CabInvoiceGenerator/src/main/java/com/bridgelabz/cabinvoicegenerator/CabInvoiceGenerator.java
@@ -1,10 +1,14 @@
 package com.bridgelabz.cabinvoicegenerator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CabInvoiceGenerator {
 
 	private static final double MINIMUM_COST_PER_KM = 10;
 	private static final int COST_PER_TIME = 1;
 	private static final double MINIMUM_FARE = 5;
+	RideRepository rideRepo = new RideRepository();
 
 	public double calculateFare(double distance, int time) {
 		double totalFare = distance * MINIMUM_COST_PER_KM + time * COST_PER_TIME;
@@ -26,6 +30,16 @@ public class CabInvoiceGenerator {
 		}
 		return new InvoiceSummary(rides.length, totalFare);
 		
+	}
+	public void addUserRide(String userId, Ride ride) {
+		rideRepo.addUsersRideDetails(userId,ride);
+		
+	}
+	public InvoiceSummary generateSummaryByUserId(String userId) {
+		ArrayList<Ride> rides = rideRepo.getRidesList(userId);
+		Ride[] ridesArray = new Ride[rides.size()];
+		ridesArray = rides.toArray(ridesArray);
+		return this.generateSummary(ridesArray);
 	}
 
 }
