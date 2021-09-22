60a7808ffd9b1e7b0db439acb709d73e13f700c2
Arpitha Bhat <arpithabht12@gmail.com>
2021-09-22 16:04:40 +0530

    ADDED: test case to validate fare given distance and time

---
+++ CabInvoiceGenerator/src/test/java/com/bridgelabz/cabinvoicegenerator/CabInvoiceGeneratorTest.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.cabinvoicegenerator;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class CabInvoiceGeneratorTest {
+	@Test
+	public void givenDistanceAndTime_ShouldReturnTotalFare() {
+		CabInvoiceGenerator invoiceGenerator = new CabInvoiceGenerator();
+		double distance = 2.0;
+		int time = 5;
+		double fare = invoiceGenerator.calculateFare(distance,time);
+		Assert.assertEquals(25, fare, 0.0);
+	}
+}
