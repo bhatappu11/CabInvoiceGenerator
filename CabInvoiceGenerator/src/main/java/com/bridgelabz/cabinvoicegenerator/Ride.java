4b68481a4795da2e4ac8599ba28c2effef9a950c
Arpitha Bhat <arpithabht12@gmail.com>
2021-09-22 21:36:08 +0530

    REFACTOR: used enums for normal and premium rides

--- CabInvoiceGenerator/src/main/java/com/bridgelabz/cabinvoicegenerator/Ride.java
+++ CabInvoiceGenerator/src/main/java/com/bridgelabz/cabinvoicegenerator/Ride.java
@@ -4,10 +4,14 @@ public class Ride {
 
 	public double distance;
 	public int time;
-
-	public Ride(double distance, int time) {
+	public RideType rideType;
+	public enum RideType {
+		PREMIUM_RIDE,NORMAL_RIDE
+	}
+	public Ride(double distance, int time,RideType type) {
 		this.distance = distance;
 		this.time = time;
+		this.rideType = type;
 	}
 
 }
